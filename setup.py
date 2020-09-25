3a9eef786b5c7c803a14df0f84b5cd4e49b18d6a
i-am-programmer-9 <peeyushpt100@gmail.com>
2020-09-25 14:25:49 +0530

    Add files via upload
    This is a setup file of Python to make a python file as a setup.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import cx_Freeze
+import sys
+import os 
+base = None
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+os.environ['TCL_LIBRARY'] = r"C:\Users\acer\AppData\Local\Programs\Python\Python38\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] = r"C:\Users\acer\AppData\Local\Programs\Python\Python38\tcl\tk8.6"
+
+executables = [cx_Freeze.Executable("snake game.py", base=base)]
+
+
+cx_Freeze.setup(
+    name = "Funny Snake game",
+    options = {"build_exe": {"packages":["tkinter","os"], "include_files":["snake-image.ico",'tcl86t.dll','tk86t.dll', 'src']}},
+    version = "1.0",
+    description = "Pygame Play and enjoy",
+    executables = executables
+    )
